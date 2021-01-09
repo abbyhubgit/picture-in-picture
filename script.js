1fb97618f43bc3440478e38889f4540312c00aaf
Hi Abby <xiaoxuhalcomb@gmail.com>
2021-01-08 20:59:21 -0600

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hi kobe');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// 
+
+async function catchMediaStream(){
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+        
+    }catch(error){
+        console.log('whoops');
+    }
+}
+
+button.addEventListener('click',async () => {
+    await videoElement.requestPictureInPicture();
+    
+});
+
+catchMediaStream();
